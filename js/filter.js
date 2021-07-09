9f62278526f6158d23133ab4563a5b526432a840
AnastasiaGer <aagermanova@gmail.com>
2021-07-08 18:05:26 -0700

    createFilter

--- js/filter.js
+++ js/filter.js
@@ -1,15 +1,19 @@
 import {createMockData} from './data.js';
 
-const mapFilters = document.querySelector('.map__filters');
-const housingType = mapFilters.querySelector('#housing-type');
-const housingPrice = mapFilters.querySelector('#housing-price');
-const housingRooms = mapFilters.querySelector('#housing-rooms');
-const housingGuests = mapFilters.querySelector('#housing-guests');
-const housingFeatures = mapFilters.querySelector('#housing-features');
+// const mapFilters = document.querySelector('.map__filters');
+// const housingType = mapFilters.querySelector('#housing-type');
+// const housingPrice = mapFilters.querySelector('#housing-price');
+// const housingRooms = mapFilters.querySelector('#housing-rooms');
+// const housingGuests = mapFilters.querySelector('#housing-guests');
+// const housingFeatures = mapFilters.querySelector('#housing-features');
 
 const createFilter = function () {
   const arrMockData = createMockData();
+  const f = arrMockData.filter((el)=> el.offer.type === 'bungalo',
+  );
+  return f;
+};
 
 
+createFilter();
 
-}
