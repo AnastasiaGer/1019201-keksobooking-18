464dd47ac11438b84af1de5a78959a7b011d1c5d
AnastasiaGer <aagermanova@gmail.com>
2021-07-09 20:16:09 -0700

    selectionchange

--- js/pins.js
+++ js/pins.js
@@ -15,6 +15,11 @@ const createPinElement = function (pinData) {
   elPin.style.left = `${pinData.location.x - PIN_WIDTH / 2  }px`;
   elPin.style.top = `${pinData.location.y + PIN_HEIGHT  }px`;
   elPin.id = pinData.offer.id;
+  elPin.setAttribute('aria-type', pinData.offer.type);
+  elPin.setAttribute('aria-price', pinData.offer.price);
+  elPin.setAttribute('aria-rooms', pinData.offer.rooms);
+  elPin.setAttribute('aria-guests', pinData.offer.guests);
+  elPin.setAttribute('aria-features', pinData.offer.features);
   const elMapPinImage = elPin.querySelector('img');
   if (elMapPinImage) {
     elMapPinImage.src = pinData.author.avatar;
@@ -24,12 +29,38 @@ const createPinElement = function (pinData) {
   return elPin;
 };
 
+const mapFilters = document.querySelector('.map__filters');
+const housingType = mapFilters.querySelector('#housing-type');
+// const housingPrice = mapFilters.querySelector('#housing-price');
+// const housingRooms = mapFilters.querySelector('#housing-rooms');
+// const housingGuests = mapFilters.querySelector('#housing-guests');
+// const housingFeatures = mapFilters.querySelector('#housing-features');
+
+var removePins = function () {
+  const mapPins = document.querySelector('.map__pins');
+  mapPins.forEach(function (item) {
+    elMap.removeChild(item);
+  });
+};
+
 const createMapPins = function () {
   const arrMockData = createMockData();
 
   if (elMapPinsArea && elPinTemplate) {
     for (let i = 0; i < arrMockData.length; i++) {
       addElementToDom(elMapPinsArea, createPinElement(arrMockData[i]));
+
+      housingType.addEventListener('selectionchange', window.debounce((evt) => {
+        if (evt.offer.type === 'bungalo') {
+          const bungaloPins = sortRandom(photos);
+          window.gallery.renderPhotosArr(randomPhotos);
+        // } else if (evt.target.id === 'filter-popular') {
+        //   window.gallery.renderPhotosArr(photos);
+        // } else if (evt.target.id === 'filter-discussed') {
+        //   const discussedPhotos = sortDiscussed();
+        //   window.gallery.renderPhotosArr(discussedPhotos);
+        // }
+      }));
     }
   }
 };
